37b91fd909df13c2bfb4a657e11728be58d58485
Francisco <javiermartincuenca@gmail.com>
2019-03-16 20:20:52 +0100

    Sign up activity implemented

--- app/src/main/java/com/azure/firechatapp/activities/login/SignUpActivity.kt
+++ app/src/main/java/com/azure/firechatapp/activities/login/SignUpActivity.kt
@@ -1,13 +1,55 @@
 package com.azure.firechatapp.activities.login
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.azure.firechatapp.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        buttonGoLogin.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
+        buttonSignUp.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if(isValidEmailAndPassword(email,password)){
+                signUpByEmail(email,password)
+            }else{
+                Toast.makeText(this,R.string.login_fill_all_data,Toast.LENGTH_SHORT);
+            }
+        }
+
+
+    }
+
+    private fun signUpByEmail(email: String, password: String){
+        mAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this) { task ->
+                if(task.isSuccessful){
+
+                }else{
+
+                }
+            }
+    }
+
+    private fun isValidEmailAndPassword(email:String, password: String): Boolean {
+        return !email.isNullOrEmpty() &&
+                !password.isNullOrEmpty() &&
+                editTextPassword.text.toString() === editTextConfirmPassword.text.toString()
     }
 }
