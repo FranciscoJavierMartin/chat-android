5960be55dd3d3912581d8306af18f592c928404c
Francisco <javiermartincuenca@gmail.com>
2019-03-17 13:03:26 +0100

    LogIn implemented

--- app/src/main/java/com/azure/firechatapp/activities/login/LoginActivity.kt
+++ app/src/main/java/com/azure/firechatapp/activities/login/LoginActivity.kt
@@ -3,11 +3,49 @@ package com.azure.firechatapp.activities.login
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.azure.firechatapp.R
+import com.azure.firechatapp.goToActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        if(mAuth.currentUser === null){
+
+        }else{
+            mAuth.signOut()
+        }
+
+        buttonLogIn.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if(isValidEmailAndPassword(email,password)){
+                logInByEmail(email,password)
+            }
+        }
+
+        textViewForgotPassword.setOnClickListener { goToActivity<ForgotPasswordActivity>() }
+        buttonCreateAccount.setOnClickListener { goToActivity<SignUpActivity>() }
+    }
+
+    private fun logInByEmail(email: String, password: String){
+        mAuth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+
+                }else{
+
+                }
+            }
+    }
+
+    private fun isValidEmailAndPassword(email: String, password: String): Boolean{
+        return !email.isNullOrEmpty() && !password.isNullOrEmpty()
     }
 }
