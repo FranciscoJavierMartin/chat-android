efb43a1b2e2dee366d8acbfad96563d323f7ab13
Francisco <javiermartincuenca@gmail.com>
2019-03-17 12:20:55 +0100

    Refactor to Sign up

--- app/src/main/java/com/azure/firechatapp/activities/login/ForgotPasswordActivity.kt
+++ app/src/main/java/com/azure/firechatapp/activities/login/ForgotPasswordActivity.kt
@@ -2,6 +2,7 @@ package com.azure.firechatapp.activities.login
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.azure.firechatapp.R
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
--- app/src/main/java/com/azure/firechatapp/activities/login/SignUpActivity.kt
+++ app/src/main/java/com/azure/firechatapp/activities/login/SignUpActivity.kt
@@ -3,8 +3,9 @@ package com.azure.firechatapp.activities.login
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.azure.firechatapp.R
+import com.azure.firechatapp.goToActivity
+import com.azure.firechatapp.toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
@@ -17,9 +18,9 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         buttonGoLogin.setOnClickListener {
-            val intent = Intent(this,LoginActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
+            goToActivity<LoginActivity> {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
         }
 
         buttonSignUp.setOnClickListener {
@@ -29,7 +30,7 @@ class SignUpActivity : AppCompatActivity() {
             if(isValidEmailAndPassword(email,password)){
                 signUpByEmail(email,password)
             }else{
-                Toast.makeText(this,R.string.login_fill_all_data,Toast.LENGTH_SHORT);
+                toast(R.string.login_fill_all_data);
             }
         }
 
@@ -40,9 +41,15 @@ class SignUpActivity : AppCompatActivity() {
         mAuth.createUserWithEmailAndPassword(email,password)
             .addOnCompleteListener(this) { task ->
                 if(task.isSuccessful){
-
+                    mAuth.currentUser!!.sendEmailVerification().addOnCompleteListener(this){
+                        toast(R.string.login_email_confirmation_sent)
+                        goToActivity<LoginActivity> {
+                            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        }
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                    }
                 }else{
-
+                    toast(R.string.login_please_try_again)
                 }
             }
     }
@@ -50,6 +57,6 @@ class SignUpActivity : AppCompatActivity() {
     private fun isValidEmailAndPassword(email:String, password: String): Boolean {
         return !email.isNullOrEmpty() &&
                 !password.isNullOrEmpty() &&
-                editTextPassword.text.toString() === editTextConfirmPassword.text.toString()
+                password === editTextConfirmPassword.text.toString()
     }
 }
