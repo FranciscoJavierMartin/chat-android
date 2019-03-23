f39a7e0a1619a4bc6d246990602aee9fee5c7b6d
Francisco <javiermartincuenca@gmail.com>
2019-03-23 11:35:28 +0100

    Login with Google

--- app/src/main/java/com/azure/firechatapp/activities/login/LoginActivity.kt
+++ app/src/main/java/com/azure/firechatapp/activities/login/LoginActivity.kt
@@ -3,12 +3,18 @@ package com.azure.firechatapp.activities.login
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.azure.firechatapp.*
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
+
 
     private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val mGoogleApiClient: GoogleApiClient by lazy { getGoogleApiClient() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,6 +61,10 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        toast(R.string.connection_failed)
+    }
+
     private fun logInByEmail(email: String, password: String){
         mAuth.signInWithEmailAndPassword(email,password)
             .addOnCompleteListener(this){ task ->
@@ -72,4 +82,15 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
+    private fun getGoogleApiClient(): GoogleApiClient {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        return GoogleApiClient.Builder(this)
+            .enableAutoManage(this,this)
+            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+            .build()
+    }
 }
